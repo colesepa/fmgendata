44752d0bbb76fa6e7810a4889da51c4705b23dff
colesepa <mjsa2005@hotmail.com>
2025-09-22 07:56:53 -0300

    feat: adicionar filedialog para selecionar arquivo

--- src/fm/app/views/main_window.py
+++ src/fm/app/views/main_window.py
@@ -3,7 +3,8 @@ from PySide6.QtWidgets import (
     QLabel, 
     QWidget, 
     QVBoxLayout,
-    QStatusBar
+    QStatusBar,
+    QFileDialog,
     )
 from PySide6.QtGui import QAction
 
@@ -29,6 +30,8 @@ class MainWindow(QMainWindow):
         menu_bar_arquivo = self.menuBar().addMenu("Arquivo")
         #Adicionando um ação a janela principal
         self.act_importar = QAction("importar HTML...", self)
+        self.act_importar.triggered.connect(self.on_import) #Add funçao _importar
+        
         #Linkando a acao de importar ao menu de importar do menubar
         menu_bar_arquivo.addAction(self.act_importar)
         
@@ -38,6 +41,17 @@ class MainWindow(QMainWindow):
         self.act_sair.triggered.connect(self.close)
         menu_bar_arquivo.addAction(self.act_sair)
         
+    def on_import(self): #Criação da função que ira capturar o path do arq
         
-        
-        
+        #QFileDialog retorno uma tupla de (<path>, <filtro_usado>)
+        path, html_filter = QFileDialog.getOpenFileName(
+            None, #Prefiro a versão sem Parent
+            "Selecione um arquivo HTML", #Título da Janela do QFileDialo
+            "", #Path inciial
+            "Arquivos HTML (*.html *htm)" #Filtro para arquivos html
+            )
+
+        #Verificar user seleção e mostrar no statusBar
+        if path: 
+            self.statusBar().showMessage(f"Arquivo selecionado: {path}", 5000)
+                
